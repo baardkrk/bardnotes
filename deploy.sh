88a4b23f9b67db7dba06e4910aab19cbcef123c0
baardkrk <baard.krohg@gmail.com>
2020-03-09 16:58:29 +0100

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,14 @@
 
 # If a command fails, then stop the deploy:
 set -e
-printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+# Setting commit message
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+        msg="$*"
+fi
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\nCommit message: \"$msg\"\n"
 
 # Build the project
 hugo -t m10c # if using a theme, add `-t <YOUR THEME>`
@@ -10,16 +17,13 @@ hugo -t m10c # if using a theme, add `-t <YOUR THEME>`
 # Add changes to GitHub
 cd public
 git add .
-msg="rebuilding site $(date)"
-if [ -n "$*" ]; then
-	msg="$*"
-fi
 git commit -m "$msg"
 
 # Push source and build repos
 git push origin master
 
 # Push changes to main repo:
+printf "\033[0;32mPushing updates to main repository...\033[0m\n"
 cd ..
 git add .
 git commit -m "$msg"
